532ac5fb0cfb09c234e13e75839c29b1ffb499cc
Miguel Angel <miguel.ampudia@outlook.es>
2023-02-27 23:50:56 -0500

    ADD DOCUMENTACION

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -5,24 +5,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class NumericApplicationTests {
 
-	@Autowired
 	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new NumericController()).build();
+	}
 
 	@Test
 	public void smallerThanOrEqualToFiftyMessage() throws Exception {
 		this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string("Smaller than or equal to 50"));
+				.andExpect(content().string("Smaller than or equal Oto 50"));
 	}
 
 	@Test
