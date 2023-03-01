33f07532afa22b36f8a961b19b5f42aebdfcd408
Miguel Angel <miguel.ampudia@outlook.es>
2023-02-28 19:09:08 -0500

    update archivos

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #cis-kubelet.sh
 
-
-total_fail=$(docker run --rm -v `pwd`:/host docker.io/aquasec/kube-bench:latest run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
 #total_fail=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
