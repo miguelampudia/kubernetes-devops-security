5aa95a965086b9e706335907ed6555d016c7fa09
Miguel Angel <miguel.ampudia@outlook.es>
2023-02-28 20:45:52 -0500

    ajuste comndos

--- cis-master.sh
+++ cis-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #cis-master.sh
-#total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
-total_fail=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+#total_fail=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
         then
