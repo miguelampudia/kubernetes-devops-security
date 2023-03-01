9127a25b2ea145e8732300ac9988497fc17afe9d
Miguel Angel <miguel.ampudia@outlook.es>
2023-03-01 00:03:33 -0500

    add prod

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service.default.svc:5000/plusone";
+	private static final String baseURL = "http://node-service:5000/plusone";
 	//private static final String baseURL = "http://localhost:57210/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
