552c9081e39e9d4864de5ed034c2f31b69faaea3
Miguel Angel <miguel.ampudia@outlook.es>
2023-02-28 21:10:11 -0500

    ajkus

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #cis-etcd.sh
 #total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets etcd  --version 1.15 --check 2.2 --json | jq .Totals.total_fail)
-total_fail=$(kube-bench run --targets etcd  --version 1.15 --check 2.2 --json | jq .Totals.total_fail)
-
-if [[ "$total_fail" -ne 0 ]];
+json_file=$(kube-bench run --targets etcd  --version 1.15 --check 2.2 --json | jq .Totals.total_fail)
+echo $json_file
+total_fail=$($json_file | jq .Totals.total_fail)
+#if [[ "$total_fail" -ne 0 ]];
+if [[ "$total_fail" -gt 0 ]];
         then
                 echo "CIS Benchmark Failed ETCD while testing for 2.2"
                 exit 1;
--- cis-master.sh
+++ cis-master.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #cis-master.sh
 #total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
-total_fail=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
-
-if [[ "$total_fail" -ne 0 ]];
+json_file=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .)
+echo $json_file
+total_fail=$($json_file | jq .Totals.total_fail) 
+#if [[ "$total_fail" -ne 0 ]];
+if [[ "$total_fail" -gt 0 ]];
         then
                 echo "CIS Benchmark Failed MASTER while testing for 1.2.7, 1.2.8, 1.2.9"
                 exit 1;
