5f47d06bc0f004fce8219a32f8181c04c5ed69a6
Miguel Angel <miguel.ampudia@outlook.es>
2023-02-28 21:20:42 -0500

    ajus

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -2,9 +2,9 @@
 #cis-kubelet.sh
 
 #total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .Totals.total_fail)
-json_file=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .Totals.total_fail)
+json_file=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json)
 echo $json_file
-total_fail=$($json_file | jq .Totals.total_fail) 
+total_fail=$(echo $json_file | jq .Totals.total_fail) 
 #if [[ "$total_fail" -ne 0 ]];
 if [[ "$total_fail" -gt 0 ]];
         then
--- cis-master.sh
+++ cis-master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #cis-master.sh
 #total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
-json_file=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .)
+json_file=$(kube-bench run --targets master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json)
 echo $json_file
-total_fail=$($json_file | jq .Totals.total_fail) 
+total_fail=$(echo $json_file | jq .Totals.total_fail) 
 #if [[ "$total_fail" -ne 0 ]];
 if [[ "$total_fail" -gt 0 ]];
         then
