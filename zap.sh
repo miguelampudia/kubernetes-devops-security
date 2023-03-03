503df3f2e32c0f9132d197e08a0ef1c205249c10
Miguel Angel <miguel.ampudia@outlook.es>
2023-03-02 22:35:19 -0500

    UPDAATE

--- zap.sh
+++ zap.sh
@@ -8,7 +8,7 @@ echo $(id -u):$(id -g)
 #docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL/v3/api-docs -f openapi -r zap_report.html
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -w report.md -J json_report.json -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL/v3/api-docs -f openapi -c zap_rules -w report.md -J json_report.json -r zap_report.html
 
 exit_code=$?
 
